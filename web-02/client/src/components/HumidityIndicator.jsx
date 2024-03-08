fcfc7839a19ecf0ad4c6a60e9b3d5957beeea202
Nicholas-Q-Smith <nicholas.quentinsmith@gmail.com>
2024-03-08 17:01:15 +0000

    Amended colour and changed value of HumidityIndicator.jsx

--- web-02/client/src/components/HumidityIndicator.jsx
+++ web-02/client/src/components/HumidityIndicator.jsx
@@ -1,24 +1,32 @@
 import React from 'react';
 
 import {
-  LinearGauge, Scale, MinorTick, Export, Title, Font,
+  LinearGauge, Scale, MinorTick, Export, Title, Font, Label, ValueIndicator,
 } from 'devextreme-react/linear-gauge';
 
 function HumidityIndicator () {
   return (
     <LinearGauge
       id="gauge"
-      value={4.3}
+      value={55}
     >
+
+    <ValueIndicator
+      color={"#FF0000"}
+    />
+  
       <Scale
         startValue={0}
         endValue={100}
         tickInterval={5}
         minorTickInterval={0.625}
       >
+  
+
+      
         <MinorTick visible={true} />
       </Scale>
-      <Export enabled={true} />
+      <Export enabled={false} />
       <Title text="Humidity">
         <Font size={28} />
       </Title>
