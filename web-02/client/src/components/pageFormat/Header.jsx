777f9ea19f9f405764b2b9d6909fa019fec7c4c3
Nicholas-Q-Smith <nicholas.quentinsmith@gmail.com>
2024-03-11 22:47:44 +0000

    Amended Header.jsx

--- web-02/client/src/components/pageFormat/Header.jsx
+++ web-02/client/src/components/pageFormat/Header.jsx
@@ -1,9 +1,11 @@
-export default function Header() {
+import '../../HomePage.css';
 
+
+export default function Header() {
   
     return (
     <header>
-      <h1>Humidity App</h1>
+      <h1 className="title">Humidity App</h1>
       <p>
         Displaying Stats of Humidity, Temp, Soil Moisture, Etc...
       </p>
