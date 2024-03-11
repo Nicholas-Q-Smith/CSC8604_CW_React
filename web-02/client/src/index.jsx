150bf0f3f4a3409f83a611c92e6700f3cc97a778
Nicholas-Q-Smith <nicholas.quentinsmith@gmail.com>
2024-03-11 22:49:22 +0000

    Changed main rendered page for SPA to App, which handles the routing through react-router-dom

--- web-02/client/src/index.jsx
+++ web-02/client/src/index.jsx
@@ -2,24 +2,41 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 
-import HomePage from './HomePage';
 import reportWebVitals from './reportWebVitals';
-
-
-
 
 import Header from './components/pageFormat/Header';
 
 import { LinearGaugeComponent } from '@syncfusion/ej2-react-lineargauge'
 
 import { IGRLinearGaugeModule, IgrLinearGauge } from 'igniteui-react-gauges';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
 
 
+
+import Layout from './components/Layout';
+import Home from './Home';
+import HomePage from './HomePage';
+import HomePage2 from './HomePage2';
+
+
+export default function App() {
+  return(
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Layout />}>
+          <Route index element={<Home />} />
+          <Route path="layout1" element={<HomePage />} />
+          <Route path="layout2" element={<HomePage2 />} />
+        </Route>
+      </Routes>
+    </BrowserRouter>
+  );
+}
 
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <HomePage />
+  <App/>
 );
 
 // If you want to start measuring performance in your app, pass a function
