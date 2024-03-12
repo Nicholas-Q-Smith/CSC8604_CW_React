67336099d3a546e39257b69f36f443a68d50fd63
Nicholas-Q-Smith <nicholas.quentinsmith@gmail.com>
2024-03-12 15:14:05 +0000

    Added Home page and styling to represent different plots to be navigated and viewed

--- web-02/client/src/Home.jsx
+++ web-02/client/src/Home.jsx
@@ -1,23 +1,50 @@
 import React from 'react';
 
-
+import { useNavigate, Route, Routes } from "react-router-dom";
 
 import Header from './components/pageFormat/Header';
 
 import Footer from './components/pageFormat/Footer';
 
+import './Home.css';
+
+
+    
+   
+
+// const NavigateToService1 = () => {
+    
+//     navigate('layout1');
+// }
+
 
 function Home(){
+    
+    let navigate = useNavigate();
 
     return (
         <>
         
         <h1>Title - Main Home Page</h1>
-        
+
+        <div class="content">
+        <div class="container">
+            <img src={require('./assets/plot shapes-01.png')} alt="Image 1" className="image"
+            onClick={() => navigate('layout1')}/>
+            <img src={require('./assets/plot shapes-02.png')} alt="Image 2" class="image"/>
+            <img src={require('./assets/plot shapes-03.png')} alt="Image 3" class="image"/>
+            <img src={require('./assets/plot shapes-04.png')} alt="Image 4" class="image"/>
+        </div>
+        </div>
         </>
         
         );
     }
+
+function LoadPlot(plotNo) {
+    
+
+}
 
 
 export default Home;
