444eecfaf21725cecc7289cefae3b3814f016759
Nicholas-Q-Smith <nicholas.quentinsmith@gmail.com>
2024-03-13 13:52:33 +0000

    Conditional rendering of soil moisture colour dependent on value

--- web-02/client/src/components/soilMoistureIndicator.jsx
+++ web-02/client/src/components/soilMoistureIndicator.jsx
@@ -5,30 +5,44 @@ import {
 } from 'devextreme-react/linear-gauge';
 
 function SoilMoistureIndicator (props) {
-return (
-    <LinearGauge
-        id="gauge"
-        value={props.moisture}
-    >
 
-    <ValueIndicator color="#FFFF00" />
-        <Scale
-            startValue={0}
-            endValue={100}
-            tickInterval={5}
-            minorTickInterval={0.625}
+    let colour = '#000000';
+
+    if(props.moisture < 20) {
+        colour = '#fa3f32';
+    } else if(props.moisture <= 50) {
+        colour = '#f7b731';
+    } else if(props.moisture <= 75) {
+        colour = '#b3ff00';
+    } else if(props.moisture <= 100) {
+        colour = '#00c417';
+    }
+    
+
+    return (
+        <LinearGauge
+            id="gauge"
+            value={props.moisture}
         >
 
+        <ValueIndicator color={colour} />
+            <Scale
+                startValue={0}
+                endValue={100}
+                tickInterval={5}
+                minorTickInterval={0.625}
+            >
 
-        
-            <MinorTick visible={true} />
-        </Scale>
-        <Export enabled={false} />
-        <Title text="Soil Moisture">
-            <Font size={28} />
-        </Title>
-    </LinearGauge>
-);
-}
+
+            
+                <MinorTick visible={true} />
+            </Scale>
+            <Export enabled={false} />
+            <Title text="Soil Moisture">
+                <Font size={28} />
+            </Title>
+        </LinearGauge>
+    );
+    }
 
 export default SoilMoistureIndicator;
