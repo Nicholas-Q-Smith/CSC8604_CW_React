ebce2b2816947ada008340d5c867c91d4bfa9449
Nicholas-Q-Smith <nicholas.quentinsmith@gmail.com>
2024-03-13 13:52:13 +0000

    Added working view indication to the top nav-bar to indicate which page is presently active

--- web-02/client/src/components/Layout.jsx
+++ web-02/client/src/components/Layout.jsx
@@ -1,4 +1,4 @@
-import { Outlet, Link, useNavigate } from "react-router-dom";
+import { Outlet, Link, useNavigate, useLocation } from "react-router-dom";
 import Tab from 'react-bootstrap/Tabs';
 import Tabs from 'react-bootstrap/Tabs';
 // import "bootstrap/dist/css/bootstrap.min.css";
@@ -9,6 +9,20 @@ import "w3-css";
 function Layout () {
 
   let navigate = useNavigate();
+
+  let activeTab = 1;
+
+  let style = "w3-bar-item w3-button w3-padding-16";
+
+  if(window.location.pathname === "/") {
+    activeTab = 0;
+  } else if(window.location.pathname === "/layout1") {
+    activeTab = 1;
+  } else if(window.location.pathname === "/layout2") {
+    activeTab = 2;
+  } else if(window.location.pathname === "/new-iter-home") {
+    activeTab = 3;
+  }
 
   return (
     <>
@@ -26,9 +40,14 @@ function Layout () {
 
 
   <div class="w3-bar w3-border w3-white w3-xxlarge w3-mobile w3-top">
-    <a onClick={()=> navigate('/')} class="w3-bar-item w3-button w3-padding-16">Home</a>
-    <a href="#" class="w3-bar-item w3-button w3-padding-16">Plot 1</a>
-    <a href="#" class="w3-bar-item w3-button w3-padding-16">Plot 2</a>
+    <a onClick={()=> navigate('/')} class=
+      {activeTab == 0 ? "w3-bar-item w3-button w3-padding-16 w3-red" : "w3-bar-item w3-button w3-padding-16 w3-white"}>Home</a>
+    <a onClick={()=> navigate('/layout1')} class=
+      {activeTab == 1 ? "w3-bar-item w3-button w3-padding-16 w3-red" : "w3-bar-item w3-button w3-padding-16 w3-white"}>Plot 1</a>
+    <a onClick={()=> navigate('/layout2')} class=
+    {activeTab == 2 ? "w3-bar-item w3-button w3-padding-16 w3-red" : "w3-bar-item w3-button w3-padding-16 w3-white"}>Plot 2</a>
+    <a onClick={()=> navigate('/new-iter-home')} class=
+    {activeTab == 3 ? "w3-bar-item w3-button w3-padding-16 w3-red" : "w3-bar-item w3-button w3-padding-16 w3-white"}>New-Iter</a>
     {/* <a href="#" class="w3-bar-item w3-button w3-padding-16">Config</a> */}
   </div>
 
