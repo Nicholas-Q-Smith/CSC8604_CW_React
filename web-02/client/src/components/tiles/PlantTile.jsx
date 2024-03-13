564b610355fe580d3ec6c258905c0270a7ed5641
Nicholas-Q-Smith <nicholas.quentinsmith@gmail.com>
2024-03-13 19:49:12 +0000

    Added placeholder content to PlantTile.jsx

---
+++ web-02/client/src/components/tiles/PlantTile.jsx
@@ -0,0 +1,19 @@
+import React from 'react';
+
+
+
+
+
+
+function PlantTile(props) {
+    let labelL = 'Left Sensor';
+    let labelR = 'Right Sensor';    
+
+    return (
+        <>
+        <h1>Plant X</h1>
+        </>
+    );
+}
+
+export default PlantTile;
