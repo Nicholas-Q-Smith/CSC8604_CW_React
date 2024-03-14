01fe8ca2420e04e782aaa0b243c6e253b507457e
Nicholas-Q-Smith <nicholas.quentinsmith@gmail.com>
2024-03-14 14:36:16 +0000

    Additional styling / format for RelevantPlants.jsx (and css)

--- web-02/client/src/components/RelevantPlants.jsx
+++ web-02/client/src/components/RelevantPlants.jsx
@@ -6,6 +6,7 @@ import "../fonts/HammersmithOne-Regular.ttf";
 import PlantTile from './tiles/PlantTile';
 
 import './RelevantPlants.css'
+import { Color } from 'devextreme-react/cjs/linear-gauge';
 
 function RelevantPlants(props) {
     let labelL = 'Left Sensor';
@@ -13,6 +14,12 @@ function RelevantPlants(props) {
 
     return (
         <>
+        <p style={{color: '#21437D'}} className={'header'}>Best Matching Plants</p>
+        <div className={'three-columns-grid'}>
+            <div className={'col1'}><PlantTile type="Col1"/></div>
+            <div className={'col2'}><PlantTile type="Col2"/></div>
+            <div className={'col3'}><PlantTile type="Col3"/></div>
+        </div>
         <div className={'three-columns-grid'}>
             <div className={'col1'}><PlantTile type="Col1"/></div>
             <div className={'col2'}><PlantTile type="Col2"/></div>
