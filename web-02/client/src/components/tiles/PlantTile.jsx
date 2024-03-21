2e332d36dc51ba62333c6add3b26d371ce26f612
Nicholas-Q-Smith <nicholas.quentinsmith@gmail.com>
2024-03-21 16:40:37 +0000

    New icon amendments to PlantTile.jsx and icon asset changes

--- web-02/client/src/components/tiles/PlantTile.jsx
+++ web-02/client/src/components/tiles/PlantTile.jsx
@@ -4,23 +4,48 @@ import Collapsible from 'react-collapsible';
 
 import './PlantTile.css';
 
-import brassicas from '../../assets/plant-tiles/plot shapes-11.png';
+// import brassicas from '../../assets/plant-tiles/plot shapes-11.png';
 
-import root_veg from '../../assets/plant-tiles/plot shapes-10.png';
+// import root_veg from '../../assets/plant-tiles/plot shapes-10.png';
 
-import leafy_greens from '../../assets/plant-tiles/plot shapes-07.png';
+// import leafy_greens from '../../assets/plant-tiles/plot shapes-07.png';
 
-import legumes from '../../assets/plant-tiles/plot shapes-07.png';
+// import legumes from '../../assets/plant-tiles/plot shapes-07.png';
 
-import fruiting_veg from '../../assets/plant-tiles/plot shapes-09.png';
+// import fruiting_veg from '../../assets/plant-tiles/plot shapes-09.png';
 
-import alliums from '../../assets/plant-tiles/plot shapes-12.png';
+// import alliums from '../../assets/plant-tiles/plot shapes-12.png';
 
-import herbs from '../../assets/plant-tiles/plot shapes-08.png';
+// import herbs from '../../assets/plant-tiles/plot shapes-08.png';
 
-import berries from '../../assets/plant-tiles/plot shapes-06.png';
+// import berries from '../../assets/plant-tiles/plot shapes-06.png';
 
-import flowers from '../../assets/plant-tiles/plot shapes-05.png';
+// import flowers from '../../assets/plant-tiles/plot shapes-05.png';
+
+
+import brassicas from '../../assets/plant-tiles-new/brassicas.png';
+
+import root_veg from '../../assets/plant-tiles-new/carrot.png';
+
+import leafy_greens from '../../assets/plant-tiles-new/leafy greens.png';
+
+import legumes from '../../assets/plant-tiles-new/carrot.png';
+
+import fruiting_veg from '../../assets/plant-tiles-new/tomato.png';
+
+import alliums from '../../assets/plant-tiles-new/onion.png';
+
+import herbs from '../../assets/plant-tiles-new/herbs.png';
+
+import berries from '../../assets/plant-tiles-new/apple.png';
+
+import flowers from '../../assets/plant-tiles-new/flowers.png';
+
+import expand from '../../assets/icons/expand.png';
+
+import collapse from '../../assets/icons/compress.png';
+
+import CollapsingIcon from '../CollapsingIcon';
 
 
 
@@ -38,9 +63,16 @@ let logos = {'Root Vegetables': root_veg,
 
             
 
+function InvertCollapse() {
+
+
+}
+
 function PlantTile(props) {
     
     let typ = props.typ;
+
+    console.log(typ)
 
     let examples = props.examples;
 
@@ -65,7 +97,7 @@ function PlantTile(props) {
     // for (let i = 0; i < examples.length; i++) {
     //     subitems.push(<p>{examples[i]}</p>);
     // }
-
+    console.log(logos[typ]);
     return (
         <>
         <div className={'plant_component'}>
@@ -77,10 +109,27 @@ function PlantTile(props) {
                     <img src={logos[typ]} alt="Image 1" className="type_image"/>
                 </div>
                 <div className={'element2'}>
-                    <Collapsible className={'plant_tile_head'} trigger={typ}>
-                        <Collapsible className={'collapsabletab'} trigger="Example Plants">
+                    <div className={"two_column_layout"}>
+                        <div className={'left'}>
+                            <Collapsible className={'plant_tile_head'} trigger={
+                            <>
+                            <div className={"two_column_layout"}>
+                                <div className={"left"}>
+                                    <p className={"expandLabel"}>{typ}</p>
+                                    </div>
+                                <div className={"right"}>
+                                    {/* <img class={'toggle_icon'} src={expand}/> */}
+                                    <CollapsingIcon isCollapsed={InvertCollapse()}/>
+                                </div>
+                                    </div>
+                                    <div>
+                                    <img/>
+                                    </div></>}>
+                        
+                            <Collapsible className={'collapsabletab'} trigger="Example Plants">
                             <p>{subitems}</p>
                         </Collapsible>
+                        
                     <div className={'two_column_layout'}>
                         <div className={'bm_left'}>
                             <p>Soil:</p>
@@ -94,6 +143,11 @@ function PlantTile(props) {
                         </div>
                     </div>
                     </Collapsible>
+                    </div>
+                    <div className={'right'}>
+                        
+                    </div>
+                    </div>
                 </div>
             </div>
 
@@ -101,10 +155,6 @@ function PlantTile(props) {
             
         
             
-        </div>
-        
-        <div className={'container'}>
-        
         </div>
         </div>
         </>
