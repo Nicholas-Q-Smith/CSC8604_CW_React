d8576a0743bcacb3101f321203b0820616051f0a
Nicholas-Q-Smith <nicholas.quentinsmith@gmail.com>
2024-03-21 16:36:42 +0000

    Removal of redundant temperature sensor details and a

--- web-02/client/src/components/DigitalTemp.jsx
+++ web-02/client/src/components/DigitalTemp.jsx
@@ -6,21 +6,23 @@ import "../fonts/HammersmithOne-Regular.ttf";
 
 
 function DigitalTemp(props) {
-    let labelL = 'Left Sensor';
-    let labelR = 'Right Sensor';    
+    let labelL = 'Ambient Temperature';
+    // let labelR = 'Right Sensor';    
+
+    if(props)
 
     return (
         <>
-        <div className="two-columns-grid">
+        {/* <div className="two-columns-grid"> */}
         <div className={'tempBoxL'}>
         <p className={'sensorLocL'}>{labelL}</p>
         <p style={{color: '#21437D'}} className={'tempIndicL'}>{props.temp}°</p>
         </div>
-        <div className={'tempBoxR'}>
+        {/* <div className={'tempBoxR'}>
         <p className={'sensorLocR'}>{labelR}</p>
         <p style={{color: '#21437D'}} className={'tempIndicR'}>{props.temp}°</p>
-        </div>
-        </div>
+        </div> */}
+        {/* </div> */}
         </>
     );
 }
