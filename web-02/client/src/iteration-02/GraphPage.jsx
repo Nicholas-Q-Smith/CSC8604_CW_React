25577c12ba8552c15f5ead464ccb6957e9e6912b
Nicholas-Q-Smith <nicholas.quentinsmith@gmail.com>
2024-03-28 19:34:22 +0000

    Added GraphPage file to manage layout shown to user

--- web-02/client/src/iteration-02/GraphPage.jsx
+++ web-02/client/src/iteration-02/GraphPage.jsx
@@ -1,10 +1,50 @@
 import React from "react";
 
-export default function GraphView() {
+import HorizontalLayoutNav from "../components/HorizontalLayoutNav";
+import Graph from "../components/Graph";
+import GraphView from "../components/Graph";
+
+import Dropdown from 'react-dropdown';
+
+import './GraphPage.css';
+
+export default function GraphPage() {
+    
+    let activeTab = 1;
+
+    const [value, setValue] = React.useState(1);
+
+    const handleChange = (event) => {
+        setValue(event.target.value);
+    }
+
+    
+    // if(window.location.pathname == "/sensors1") {
+    //     activeTab = 1;
+    // } else if(window.location.pathname == "/sensors2") {
+    //     activeTab = 2;
+    // }
+
     return (
-        <div>
-            <Graph></Graph>
-        </div>
+        <>
+            <HorizontalLayoutNav/>
+            <div>
+            <div class={'dropDown'}>
+
+            <select style={{width: '400px', height: '100px'}} value={value} onChange={handleChange}>
+
+            <option value={1}>Plot 1 Graphs</option>
+
+            <option value={2}>Plot 2 Graphs</option>
+
+            </select>
+
+            </div>
+            <div class={'graphContainer'}>
+            <Graph activeTab={Number(value)}></Graph>
+            </div>
+            </div>
+        </>
 
     )
 } 
