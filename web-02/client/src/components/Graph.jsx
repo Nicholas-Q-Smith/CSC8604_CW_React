8090e4864a11284a6714c910ad53286b410f504d
Nicholas-Q-Smith <nicholas.quentinsmith@gmail.com>
2024-03-28 19:33:11 +0000

    Added iframes for both sets of sensor data

--- web-02/client/src/components/Graph.jsx
+++ web-02/client/src/components/Graph.jsx
@@ -1,20 +1,42 @@
 import React from "react";
 
-export default function GraphView() {
-    return (
-        <div>
-            <iframe width="450" height="260" style={{border: '1px solid #cccccc'}} 
-            src="https://thingspeak.com/channels/2455916/charts/1?bgcolor=%23ffffff&color=%23d62020&dynamic=true&results=60&type=line&update=15">
+export default function Graph(props) {
 
-            </iframe>
+    if(props.activeTab == 1) {
 
-            <iframe width="450" height="260" style={{border: '1px solid #cccccc'}} 
-            src="https://thingspeak.com/channels/2455916/charts/2?bgcolor=%23ffffff&color=%23d62020&dynamic=true&results=60&type=line&update=15">
+        return (
+            <div>
+                <iframe width="450" height="260" style={{border: '1px solid #cccccc'}} 
+                src="https://thingspeak.com/channels/2455916/charts/1?bgcolor=%23ffffff&color=%23d62020&dynamic=true&results=60&type=line&update=15">
 
-            </iframe>
+                </iframe>
 
-            <iframe width="450" height="260" style={{border: '1px solid #cccccc'}} 
-            src="https://thingspeak.com/channels/2455916/charts/3?bgcolor=%23ffffff&color=%23d62020&dynamic=true&results=60&type=line&update=15"></iframe>
-        </div>
-    );
+                <iframe width="450" height="260" style={{border: '1px solid #cccccc'}} 
+                src="https://thingspeak.com/channels/2455916/charts/2?bgcolor=%23ffffff&color=%23d62020&dynamic=true&results=60&type=line&update=15">
+
+                </iframe>
+
+                <iframe width="450" height="260" style={{border: '1px solid #cccccc'}} 
+                src="https://thingspeak.com/channels/2455916/charts/3?bgcolor=%23ffffff&color=%23d62020&dynamic=true&results=60&type=line&update=15"></iframe>
+
+            </div>
+        );
+    } else if(props.activeTab == 2) {
+        return (
+            <>
+            <div>
+                <iframe width="450" height="260" style={{border: '1px solid #cccccc'}} 
+                src="https://thingspeak.com/channels/2480580/charts/1?bgcolor=%23ffffff&color=%23d62020&dynamic=true&results=60&type=line&update=15"></iframe>
+
+                <iframe width="450" height="260" style={{border: '1px solid #cccccc'}} 
+                src="https://thingspeak.com/channels/2480580/charts/2?bgcolor=%23ffffff&color=%23d62020&dynamic=true&results=60&type=line&update=15"></iframe>
+
+                <iframe width="450" height="260" style={{border: '1px solid #cccccc'}} 
+                src="https://thingspeak.com/channels/2480580/charts/3?bgcolor=%23ffffff&color=%23d62020&dynamic=true&results=60&type=line&update=15"></iframe>
+
+            </div>
+            </>
+            
+        );
+    }
 }
