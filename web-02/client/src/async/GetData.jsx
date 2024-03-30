ca66d2c077c95462e461ae3f40667d3af29e6280
Nicholas-Q-Smith <nicholas.quentinsmith@gmail.com>
2024-03-30 17:06:04 +0000

    Commenting and tidying for GetData.jsx

--- web-02/client/src/async/GetData.jsx
+++ web-02/client/src/async/GetData.jsx
@@ -2,57 +2,50 @@ import React from "react";
 
 
 export default function GetData() {
+
+    // for getting api verification
     const [data, setData] = React.useState(null);
 
+    // for getting sensor data JSON object
     const [vals, setVals] = React.useState({});
 
-    const [vals2, setVals2] = React.useState({});
-
+    // for getting plant data JSON object
     const [plantvals, setPlantVals] = React.useState({});
 
     React.useEffect(() => {
 
-      // const interval = setInterval(GetData, 30000);
 
+      //api verification
       async function fetchStatus() {
         const response = await fetch("/api");
         const json = await response.json();
         setData(json.message);
       }
 
-      
-
+      //sensor data
       async function fetchSensors() {
         const response = await fetch("/sensors");
         const json = await response.json();
         setVals(json);
       }
 
-    
-      // async function fetchBestMatch() {
-      //   const response = await fetch("/get-best-match");
-      //   const json = await response.json();
-      //   setPlantVals({items: plantvals});
-       
-      // }
-
+      //plant data
       async function fetchBestMatch() {
         try {
           const response = await fetch("/get-best-match");
           const json = await response.json();
-          setPlantVals({items: json}); // Use `json` here instead of `plantvals`
+          setPlantVals({items: json}); 
         } catch (error) {
           console.error("Error fetching data:", error);
         }
       }
-
-      
-
+    
       fetchStatus();
       fetchSensors();
       fetchBestMatch();
-      // return () => clearInterval(interval);
+      
     } , []);
-    console.log("Returning: " + data, vals, plantvals);
+
+    //returning all data together
     return {data, vals, plantvals};
 }
