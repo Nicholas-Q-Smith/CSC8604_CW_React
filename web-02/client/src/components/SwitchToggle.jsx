536afdb801219d01f0cc1f2d0fd70eaca8d062e3
Nicholas-Q-Smith <nicholas.quentinsmith@gmail.com>
2024-03-30 19:46:48 +0000

    Tidied SwitchToggle.js coe and added comments

--- web-02/client/src/components/SwitchToggle.jsx
+++ web-02/client/src/components/SwitchToggle.jsx
@@ -4,10 +4,11 @@ import Switch from 'react-switch';
 
 import { Component } from 'react'
 
-
+/* This defines the object class for the auto-update switch
+*  which is used in the navbar to toggle the auto-update
+*/
 
 class SwitchToggle extends Component {
-
 
     constructor(props) {
       super(props);
@@ -15,31 +16,24 @@ class SwitchToggle extends Component {
       this.handleChange = this.handleChange.bind(this);
       
     }
-
-  
+    
+    /*Internal functon to set
+    state to the current value of the switch
+    */
     handleChange(checked) {
       this.setState({ checked });
-
-
       this.props.onToggle(checked);
     }
-
     
-
-    isAutoUpdate() {
-      return this.state.checked;
-    }
-  
+    /*Render function to display the switch
+    */
     render() {
       return (
         <label>
           <span>Auto Update</span>
-          {/* <Switch onChange={this.handleChange} checked={this.state.checked} /> */}
           <Switch onChange={this.handleChange} checked={this.state.checked} />
         </label>
       );
-    }
-
-    
+    }  
   }
   export default SwitchToggle;
